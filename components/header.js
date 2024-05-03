5d36127c8e6f1de878561e39ff07548c94e1f516
Ilekaede <asakuramotiyo@gmail.com>
2024-05-03 19:04:01 +0900

    add:container.module.css

--- components/header.js
+++ components/header.js
@@ -1,13 +1,16 @@
 import Logo from "./logo";
 import Nav from "./nav";
 import styles from "../src/styles/header.module.css";
+import Container from "./container";
 export default function Header() {
   return (
     <header>
-      <div className={styles.flexContainer}>
-        <Logo boxOn />
-        <Nav />
-      </div>
+      <Container large>
+        <div className={styles.flexContainer}>
+          <Logo boxOn />
+          <Nav />
+        </div>
+      </Container>
     </header>
   );
 }
