c441e81dd2bffe5e27e46e971551e1b2d1891ff6
Ilekaede <asakuramotiyo@gmail.com>
2024-05-04 15:40:27 +0900

    SNSアイコンを追加

--- components/contact.js
+++ components/contact.js
@@ -1,10 +1,11 @@
 import styles from "src/styles/contact.module.css";
-
+import Social from "./social";
 export default function Contact() {
   return (
     <div className={styles.stack}>
       <h3 className={styles.heading}>Contact</h3>
-      <address>saji-kenta@gmail.com</address>
+      <Social iconsize="30px" />
+      <address>saji-kenta@sajilove.com</address>
     </div>
   );
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import Layout from "components/layout";
 import "../styles/globals.css";
+
+//font awesomeの設定
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
