ef22260bd4ac1ed90da35e70e06f0b124bba3770
Ilekaede <asakuramotiyo@gmail.com>
2024-05-05 21:25:05 +0900

    ハンバーガーメニューの作成

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,27 @@
 import Container from "components/container";
 import Hero from "../../components/hero";
 import Meta from "components/meta";
-export default function Home() {
+import { getAllPosts } from "lib/api";
+import Posts from "components/posts";
+import Pagination from "components/pagination";
+
+export default function Home({ posts }) {
   return (
-    <>
-      <Container>
-        <Meta />
-        <Hero title={"SAJI"} subtitle={"アウトプットしていくサイト"} imageOn />
-      </Container>
-    </>
+    <Container>
+      <Meta />
+      <Hero title={"SAJI"} subtitle={"アウトプットしていくサイト"} imageOn />
+      <Posts posts={posts} />
+      <Pagination nextUrl="/blog" nextText="More Posts" />
+    </Container>
   );
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts(4);
+
+  return {
+    props: {
+      posts: posts,
+    },
+  };
 }
