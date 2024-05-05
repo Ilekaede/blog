90609bfda8665d0b9d03948cd206a2a3b2335925
Ilekaede <asakuramotiyo@gmail.com>
2024-05-05 19:02:51 +0900

    タイトルフォントが小さい問題を修正，カンマをピリオドで打っていた

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,11 +1,25 @@
 import Container from "components/container";
 import Hero from "components/hero";
 import Meta from "components/meta";
-export default function Blog() {
+import { getAllPosts } from "lib/api";
+import Posts from "components/posts";
+
+export default function Blog({ posts }) {
   return (
     <Container>
       <Meta pageTitle="ブログ" pageDesc="ブログの記事一覧" />
       <Hero title={"Blog"} subtitle={"Recent Post"} />
+      <Posts posts={posts} />
     </Container>
   );
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts();
+
+  return {
+    props: {
+      posts: posts,
+    },
+  };
 }
